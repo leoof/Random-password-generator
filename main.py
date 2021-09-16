c4fd15624f74d7476ddacd890dd8257107c87f76
leoof <45597335+leoof@users.noreply.github.com>
2021-09-16 14:54:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+import time
+
+sequence = ["q","w","e","r","t","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9",",",".","/","!","£","$","%","^","&","*","(",")","-","+","=","~","@","'",";",":","?","<",">","¬","|"]
+final = []
+
+letters = int(input("How many characters would you like to have in your password? "))
+
+for x in range(letters):
+  final.append(random.choice(sequence))
+
+print(''.join(final))
